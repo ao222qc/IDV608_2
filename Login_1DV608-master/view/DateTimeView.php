f7d620aa0d0649ef546d9a4fb6a92b376fb1d2e3
ao222qc <ao222qc@student.lnu.se>
2015-09-08 13:55:35 +0200

    Date and time added
    Fantastic progress.

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -5,7 +5,14 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		date_default_timezone_set("Europe/Stockholm");
+
+		$weekDay = date('l');
+		$numericDate = date('j');
+		$month = date('F');
+		$time = date('y:h:sa');
+
+		$timeString = $weekDay . " the " . $numericDate . "th of " . $month . ". The time is : " . $time;
 
 		return '<p>' . $timeString . '</p>';
 	}
